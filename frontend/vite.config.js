f43c1d27295b8f29e65494773eb7807aef393b87
matiassromo <matiasromo10@hotmail.com>
2024-10-13 17:26:02 -0500

    requirements and vite.config.js fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  //root: 'frontend', // Asegúrate de que esto apunte a la carpeta correcta
   plugins: [vue()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://proyectoweb-bmeqh6ftezb4cwh2.canadacentral-01.azurewebsites.net',
+        target: 'http://localhost:5000', // Cambia a tu backend local si es necesario
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
