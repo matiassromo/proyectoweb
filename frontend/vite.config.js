5752f317a678a6fdb130d6952c159bda2d9e26eb
matiassromo <matiasromo10@hotmail.com>
2024-10-14 19:17:27 -0500

    more fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+
   plugins: [vue()],
   build: {
-    root: 'frontend',
-    outDir: 'dist' // Esto es correcto
+    outDir: 'dist' // Generar los archivos en frontend/dist
   },
 });
