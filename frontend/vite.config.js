5a726958e9cab5ae657b07783a4a32f91a5bc983
matiassromo <matiasromo10@hotmail.com>
2024-10-14 23:43:39 -0500

    vercel deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Alias para las rutas de componentes
+    },
+  },
   build: {
     outDir: 'dist'
   },
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000', // Usado solo en desarrollo
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
